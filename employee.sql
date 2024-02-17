a895105aee78350477242d6c00ab94786ac1d1d1
Sandeep Thakur <117976443+geeksandip@users.noreply.github.com>
2024-02-17 14:54:03 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,25 +1,25 @@
 SELECT * FROM project.employee;
 
--- Retrieve alll employee in the Engineering department 
+-- Retrieve all employee in the Engineering department 
 select * from Employee where Department ='Engineering';
 
 -- Retrieve employee with a salary greater than 60000
 select * from Employee where Salary>60000;
 
--- Calcualate the average salary of employee
+-- Calculate the average salary of the employee
 select avg(Salary) from Employee;
 
--- Calcuklating Average Salary by Department
+-- Calculating Average Salary by Department
 select Department,  avg(Salary) as AvgSalary
 from Employee
 group by Department;
 
--- Counting Employee by Gender and Deparment
+-- Counting Employees by Gender and department
 select Department, Gender,  count(*) as Count
 from Employee
 group by Department, Gender;
 
--- Finding the Highhest Paid Employee;
+-- Finding the Highest Paid Employee;
 select Department, Position, Salary
 From Employee
 order by Salary desc
@@ -54,7 +54,7 @@ from Employee
 group by StartYear
 order by StartYear;
 
--- Calculating Male to Female ration by Department
+-- Calculating Male to Female ratio by Department
 select Department,
 	sum(case when Gender ='Male' then 1 else 0 end ) as MaleCount,
     sum(case when Gender = 'Female' then 1 else 0 end) as FemaleCount
@@ -65,7 +65,7 @@ group by department;
 select (count(case when StartDate is not null then 1 end)*100.0)/count(*) as TurnoverRate
 from Employee;
 
--- Calculating Avverage Age by Department and Gender
+-- Calculating Average Age by Department and Gender
 select Department, Gender, avg(Age) as AvgAge
 from Employee
 group by Department, Gender;
@@ -78,7 +78,7 @@ from Employee
 group by StartYear
 order by StartYear;
 
--- Identiffying most common Age Group Among Manager
+-- Identifying most common Age Group Among Managers
 select Position, AgeGroup, count(*) as count
 from (
 select *,
@@ -101,7 +101,7 @@ select Gender,
 from Employee
 group by Gender;
 
--- Finding Employee with Frequent jon Changes
+-- Finding Employee with Frequent Jon Changes
 select FirstName, LastName, count(distinct Position) as PositionChange
 from Employee
 group by EmployeeID
@@ -119,21 +119,21 @@ select Department, avg(age) as AvgAgeWhenJoined
 from Employee
 group by Department;
 
--- Calculatng Deepatment Wise employee ang range
+-- Calculating Department Wise employee and range
 select Department,
 	max(Age) as MaxAge,
     min(Age) as MinAge
 from Employee
 group by Department;
 
--- Identifying Employee with Frequent Department Change
+-- Identifying Employees with Frequent Department Change
 select FirstName, LastName, count(distinct Department) as DepartmentChanges
 from Employee
 group by EmployeeID
 having DepartmentChanges>1
 order by DepartmentChanges desc;
 
---  Calculating Department wise Salary Distribution
+--  Calculating Department Salary Distribution
 select Department,
 	count(*) as EmployeeCount,
     avg(Salary) as AvgSalary
